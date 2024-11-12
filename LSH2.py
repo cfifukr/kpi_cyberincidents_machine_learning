e43a146972e9e4690a3e7427eaa528e675e27454
Vladyslav PC Kyiv <prosto.acc.vlada@gmail.com>
2024-11-13 01:16:33 +0200

    lab7

--- LSH2.py
+++ LSH2.py
@@ -6,11 +6,10 @@ from tensorflow.keras.applications import ResNet50
 from datasketch import MinHash, MinHashLSH
 from sklearn.preprocessing import Binarizer
 
-# Ініціалізація моделі для витягання фіч
+# модель для витягання фіч
 model = ResNet50(weights='imagenet', include_top=False, pooling='avg')
 
 
-# Функція для витягання фіч зображення
 def extract_features(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
     img_data = image.img_to_array(img)
@@ -20,13 +19,12 @@ def extract_features(img_path, model):
     return features.flatten()
 
 
-# Функція для перетворення вектора фіч на MinHash об'єкт
+# перетворення вектора фіч на MinHash об'єкт
 def create_minhash(vector, num_perm=64):
-    # Перетворюємо вектор на бінарний формат
+    # перетворюємо вектор на бінарний формат
     binarizer = Binarizer(threshold=0.0)
     binary_vector = binarizer.fit_transform(vector.reshape(1, -1))[0]
 
-    # Створюємо MinHash
     m = MinHash(num_perm=num_perm)
     for idx, val in enumerate(binary_vector):
         if val > 0:
@@ -34,7 +32,6 @@ def create_minhash(vector, num_perm=64):
     return m
 
 
-# Завантаження фіч з тренувального набору і збереження в MinHashLSH
 def load_dataset_to_lsh(folder_path, lsh, num_perm=64):
     labels = []
     for label_folder in os.listdir(folder_path):
@@ -52,16 +49,13 @@ def load_dataset_to_lsh(folder_path, lsh, num_perm=64):
     return labels
 
 
-# Ініціалізація MinHashLSH
 num_perm = 64
 lsh = MinHashLSH(threshold=0.5, num_perm=num_perm)
 
-# Завантаження тренувальних фіч у LSH з папки ./Animals
 train_folder = './Animals'
 labels = load_dataset_to_lsh(train_folder, lsh, num_perm)
 
 
-# Функція для ідентифікації зображень з тестової папки ./Test
 def classify_test_images(test_folder, lsh, labels, num_perm=64):
     test_results = {}
     label_dict = dict(labels)
@@ -70,10 +64,9 @@ def classify_test_images(test_folder, lsh, labels, num_perm=64):
         img_path = os.path.join(test_folder, img_file)
         test_feature = extract_features(img_path, model)
 
-        # Створюємо MinHash для тестового зображення
         test_minhash = create_minhash(test_feature, num_perm)
 
-        # Пошук найближчих сусідів у LSH
+        # пошук найближчих сусідів у LSH
         matches = lsh.query(test_minhash)
 
         if matches:
@@ -87,6 +80,5 @@ def classify_test_images(test_folder, lsh, labels, num_perm=64):
     return test_results
 
 
-# Ідентифікуємо тестові зображення
 test_folder = './Test'
 results = classify_test_images(test_folder, lsh, labels, num_perm)
